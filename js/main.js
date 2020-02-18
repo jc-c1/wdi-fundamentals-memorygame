7e71f3e4cf3898c88fab3b8ed79143aad636e2c4
Joanne Chang <joannec.chang1@gmail.com>
2020-02-18 01:38:48 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-let cards = ["queen", "queen", "king", "king"];
+const cards = [
+{rank:"Queen", suit:"hearts", cardImage:"images/queen-of-hearts.png"},
+{rank:"Queen", suit:"diamonds", cardImage:"images/queen-of-diamonds.png"},
+{rank:"King", suit:"hearts", cardImage:"images/king-of-hearts.png"},
+{rank:"King", suit:"diamonds", cardImage:"images/king-of-diamonds.png"} ]
 
 let cardsInPlay = [];
 
@@ -10,8 +14,10 @@ var checkForMatch = function () {
 };
 
 var flipcard = function (cardId) {
-	console.log ("User flipped " + cards[cardId]);
-	cardsInPlay.push (cards[cardId]);
+	console.log ("User flipped " + cards[cardId].rank);
+	console.log ("User flipped " + cards[cardId].suit);
+	console.log ("User flipped " + cards[cardId].cardImage);
+	cardsInPlay.push (cards[cardId].rank);
 
 };
 
