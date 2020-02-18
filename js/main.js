0fea06263edeae88038ca22ead8712323f87603f
Joanne Chang <joannec.chang1@gmail.com>
2020-02-18 01:25:54 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,21 +2,24 @@ let cards = ["queen", "queen", "king", "king"];
 
 let cardsInPlay = [];
 
-let cardOne = cards[0];
-let cardTwo = cards[1];
+var checkForMatch = function () {
+	if (cardsInPlay[0]===cardsInPlay[1]) {
+			console.log("You found a match!");
+		} else {console.log("Sorry, try again.");
+	};
+};
 
+var flipcard = function (cardId) {
+	console.log ("User flipped " + cards[cardId]);
+	cardsInPlay.push (cards[cardId]);
 
-cardsInPlay.push(cardOne);
+};
 
+flipcard (0);
 
-console.log("User flipped " + cardsInPlay[0]);
+flipcard (1);
 
-cardsInPlay.push(cardTwo);
+	checkForMatch();
 
-console.log("User flipped " + cardsInPlay[1]);
-
-if (cardsInPlay[0]===cardsInPlay[1]) {
-	alert("You found a match!");
-} else {alert("Sorry, try again.");}
 
 
